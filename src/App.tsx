c4b034c77f420843ddb7fe2a31c36a42226b8500
Asif Mohammed <asifm2882@gmail.com>
2019-11-03 15:04:05 +0530

    add RollDice to App.tsx :hammer:

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import RollDice from './dies-roll-game/RollDice/RollDice';
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RollDice />
+    </div>
+  );
 };
 
 export default App;
