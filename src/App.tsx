709e53a46cf3d0dda053ec2f576282b75562de76
Asif Mohammed <asifm2882@gmail.com>
2019-11-03 18:21:13 +0530

    add roll dice feature :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import RollDice from './dies-roll-game/RollDice/RollDice';
+
 const App: React.FC = () => {
   return (
     <div className="App">
--- src/dies-roll-game/RollDice/RollDice.tsx
+++ src/dies-roll-game/RollDice/RollDice.tsx
@@ -1,27 +1,40 @@
 import React, { useState } from 'react';
 import Die from '../Die/Die';
+import styled from 'styled-components';
+import { Button } from 'antd';
 
-type state = { [key: string]: string };
+const Buttons = styled.div`
+  align-content: center;
+`;
+
+type Diestate = { [key: string]: string };
+type state = { die: Diestate; loading: boolean };
+const intialState = { die: { die1: 'two', die2: 'four' }, loading: false };
 const fontFaces = ['one', 'two', 'three', 'four', 'five', 'six'];
 const genRandFace = () =>
   fontFaces[Math.floor(Math.random() * fontFaces.length)];
 
 const RollDice = () => {
-  const intialState = { die1: 'two', die2: 'four' };
   const [dice, setDice] = useState<state>(intialState);
 
   const rollDice = () => {
-    let newState: typeof dice = {};
-    Object.keys(dice).forEach(key => (newState[key] = genRandFace()));
-    setDice(newState);
+    let newState: typeof dice = { die: {}, loading: false };
+    setDice({ ...dice, loading: true });
+    Object.keys(dice.die).forEach(key => (newState.die[key] = genRandFace()));
+    setTimeout(() => {
+      setDice(newState as state);
+    }, 800);
   };
   return (
     <div>
-      {Object.keys(dice).map((key, i) => (
-        <Die key={i} face={dice[key]} />
+      {Object.keys(dice.die).map((key, i) => (
+        <Die key={i} face={dice.die[key]} animate={dice.loading} />
       ))}
-
-      <button onClick={rollDice}>roll dice</button>
+      <Buttons>
+        <Button type="primary" onClick={rollDice} loading={dice.loading}>
+          roll dice
+        </Button>
+      </Buttons>
     </div>
   );
 };
