ea2a328351c3314aec1c7d8631888760aa77defe
Asif Mohammed <asifm2882@gmail.com>
2019-11-03 21:46:04 +0530

    add more dice through input

--- src/dies-roll-game/Die/Die.tsx
+++ src/dies-roll-game/Die/Die.tsx
@@ -1,39 +1,11 @@
 import React from 'react';
-import styled, { keyframes, css } from 'styled-components';
+import styled from 'styled-components';
 const DieContiner = styled.i`
   font-size: 10rem;
   padding: 0.4rem;
   color: #0e467a;
 `;
 
-const shake = keyframes`
-  from,
-  to {
-    transform: translate3d(0, 0, 0);
-  }
-
-  10%,
-  30%,
-  50%,
-  70%,
-  90% {
-    transform: translate3d(-10px, 0, 0);
-  }
-
-  20%,
-  40%,
-  60%,
-  80% {
-    transform: translate3d(10px, 0, 0);
-  }
-`;
-
-const shakeAnimate = css`
-  animation: ${shake} 0.8s linear;
-`;
-const noAnimate = css`
-  animation: '';
-`;
 type DieProps = { face: string; animate: boolean };
 const Die = ({ face, animate }: DieProps) => {
   return (
--- src/dies-roll-game/RollDice/RollDice.tsx
+++ src/dies-roll-game/RollDice/RollDice.tsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 import Die from '../Die/Die';
-import { Button } from 'antd';
+import { Button, InputNumber } from 'antd';
+import styled from 'styled-components';
 
 type Diestate = { [key: string]: string };
 // type state = { die: Diestate; loading: boolean };
+
+const StyledButton = styled(Button)`
+  margin-left: 20px;
+`;
 const intialState = { die1: 'two', die2: 'four' };
 const fontFaces = ['one', 'two', 'three', 'four', 'five', 'six'];
+let inputRef: InputNumber | null;
 const genRandFace = () =>
   fontFaces[Math.floor(Math.random() * fontFaces.length)];
 
@@ -22,15 +28,37 @@ const RollDice = () => {
       setLoading(false);
     }, 800);
   };
+
+  const onInputChange = (val: number | undefined) => {
+    if (val) {
+      // eslint-disable-next-line no-sequences
+      let newState = Array(val)
+        .fill(null)
+        .reduce(
+          (acc, curr, i) => ((acc[`die${i + 1}`] = genRandFace()), acc),
+          {}
+        );
+      console.log(newState);
+      setDice(newState);
+    }
+  };
   return (
     <div>
       {Object.keys(dice).map((key, i) => (
         <Die key={i} face={dice[key]} animate={loading} />
       ))}
       <div>
-        <Button type="primary" onClick={rollDice} loading={loading}>
+        <InputNumber
+          min={1}
+          max={6}
+          defaultValue={2}
+          onChange={onInputChange}
+          ref={input => (inputRef = input)}
+          disabled={loading}
+        />
+        <StyledButton type="primary" onClick={rollDice} loading={loading}>
           Roll Dice
-        </Button>
+        </StyledButton>
       </div>
     </div>
   );
